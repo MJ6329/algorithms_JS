32a2dd3547edd657955eaaec65910261347b5b3d
MJ <mj632900@gmail.com>
2022-04-30 22:39:15 +0900

    시험 성적

--- 1330/app.js
+++ 1330/app.js
@@ -2,9 +2,9 @@ let fs = require('fs');
 let [a,b] = fs.readFileSync('./dev/stdin').toString().split(' ').map(v=>parseInt(v));
 
 if(a>b){
-    console.log('>')
+    console.log('>');
 } else if(a<b){
-    console.log('<')
+    console.log('<');
 } else{
-    console.log('==')
+    console.log('==');
 }
