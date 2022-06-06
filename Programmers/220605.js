f811d5b298b8216117de7283dafa6009376ef47f
MJ <mj632900@gmail.com>
2022-06-07 00:13:41 +0900

    Rechallenge: fail

--- Programmers/220605.js
+++ Programmers/220605.js
@@ -5,21 +5,22 @@ const speeds = [1, 1, 1, 1, 1, 1];
 solution(progresses, speeds)
 
 // level2는 아직 나에겐 어렵다 나중에 다시 도전.
-
+// 코드를 참고해서 작성했는데도 테스트2에서 실패가 뜬다 왤까?
 function solution(progresses, speeds) {
     let answer = [];
-    let complete = 1;
+    let complete = 0;
+    let cnt = 1;
     let days = progresses.map((p, s) => Math.ceil(100-p) / speeds[s]);
-    for(let i=0; i<days.length; i++){
-        if(days[0] <= days[1]){
-            answer.push(complete);
-            days.shift(days[0]);
-            console.log(days)
+    for(let i=1; i<days.length; i++){
+        console.log(days[complete], days[i], days.length, i)
+        if(days[complete] < days[i]){
+            answer.push(cnt);
+            complete += cnt;
+            cnt = 1;
         }else {
-            complete++;
-            days.shift(days[1]); // shift는 맨 앞요소만 제거 할 수 있다는걸 잊고 있었다.
-            console.log(days)
+            cnt++;
         }
+        if(i === days.length-1) answer.push(cnt)
     }
     console.log(answer)
        
