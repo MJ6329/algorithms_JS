bcd78bad7027946c58f48397d73b299b46cd2440
MJ <mj632900@gmail.com>
2022-06-26 20:18:03 +0900

    Refactoring Programmers Level2

--- Programmers/Kit/스택,큐/220605.js
+++ Programmers/Kit/스택,큐/220605.js
@@ -4,29 +4,20 @@ const speeds = [1, 1, 1];
 
 solution(progresses, speeds)
 
-// 코드를 참고해서 작성했는데도 테스트2에서 실패가 뜬다 왤까?
-// Math.ceil(100-p) / speeds[s]); => Math.ceil((100-p) / speeds[s])); 괄호를 하나 빼 먹었다..
-// 멍청한 실수 였음 
-
 function solution(progresses, speeds) {
     let answer = [];
-    let complete = 0;
-    let cnt = 1;
-    let days = progresses.map((p, s) => Math.ceil((100-p) / speeds[s]));
-    for(let i=1; i<days.length; i++){
-        console.log(days[complete], days[i], days.length, i)
-        if(days[complete] < days[i]){
-            answer.push(cnt);
-            console.log('com+cnt', cnt)
-            complete += cnt;
-            console.log(complete)
-            cnt = 1;
-            console.log('resetcnt', cnt)
+    let count = 1;
+    let complete = progresses.map((item, idx) => Math.ceil((100-item) / speeds[idx]));
+    let days = complete[0];
+    for(let i=1; i<=complete.length; i++){
+        if(days < complete[i]){
+            answer.push(count);
+            days = complete[i];
+            count = 1;
         }else {
-            cnt++;
+            count++;
         }
-        if(i === days.length-1) answer.push(cnt)
+        if(i === complete.length-1) answer.push(count);
     }
-    console.log(answer)
-       
+    console.log(answer);
 }
