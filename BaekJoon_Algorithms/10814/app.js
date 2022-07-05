7efca68778602384ff9b004824c4d05d908db6f3
MJ <mj632900@gmail.com>
2022-07-06 00:25:33 +0900

    나이순 정렬

--- BaekJoon_Algorithms/10814/app.js
+++ BaekJoon_Algorithms/10814/app.js
@@ -1,19 +1,13 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().split('\n');
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 solution(input);
 
+// 간단하게 풀 수 있었던 문제였는데 꼬아서 생각했다
+// 문자열로 된 숫자 ex)'22' 등 도 정렬이 되는 것을 알았다.
 function solution(N) {
-    let a = {};
-    const num = +N[0];
-    for(let i=1; i<=num; i++) {
-        let [age, name] = N[i].split(' ');
-        a[name] = +age;
-    }
-    let sortArr = Object.entries(a).sort((a,b) => a[1] - b[1]);
-    for(let i=0; i<num; i++){
-       let [name, age] = sortArr[i].join(' ').split(' ');
-       console.log(age+' '+name)
-    }
+    N.shift();
+    N.sort((a,b) => a.split(' ')[0] - b.split(' ')[0]);
+    console.log(N.join('\n'))
 }
